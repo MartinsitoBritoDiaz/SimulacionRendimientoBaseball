28b7954cb19c7bb697ed796d869250ec0a81aeed
Martinsito Brito Diaz <martinsito_brito@ucne.edu.do>
2021-03-17 01:29:39 -0400

    Cambios

--- SimulacionRendimientoBasseball/UI/Simulador.cs
+++ SimulacionRendimientoBasseball/UI/Simulador.cs
@@ -8,11 +8,11 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace SimulacionRendimientoBasseball
+namespace SimulacionRendimientoBasseball.UI
 {
-    public partial class Form1 : Form
+    public partial class Simulador : Form
     {
-        public Form1()
+        public Simulador()
         {
             InitializeComponent();
         }
